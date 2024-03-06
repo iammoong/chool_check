1eeea13d053673ca40438f3002f3353d4cb10daf
iammoong <ansrlwkd225@naver.com>
2024-03-06 21:30:06 +0900

    지도 적용.. 안됨..

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,15 +9,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // latitude - 위도, longitude - 경도
+  static final LatLng companyLatLng = LatLng(
+      37.5233273,
+      126.921252
+  );
+  static final CameraPosition initialPosition =
+      CameraPosition(
+          target: companyLatLng,
+          zoom: 15);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Home Screen'
-        ),
+      body: GoogleMap(
+
+        mapType: MapType.terrain,
+       initialCameraPosition: initialPosition,
+
       ),
     );
   }
 }
-
